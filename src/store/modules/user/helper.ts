7961001426952541d64afd4d29ec80237f56c512
wrtpi <71971470+wrtpi@users.noreply.github.com>
2023-03-02 16:49:35 +0800

    Create helper.ts

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,31 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://raw.githubusercontent.com/wrtpi/test/main/src/assets/avatar.jpg',
+      name: 'ChatGPT',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
